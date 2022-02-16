769f436dd6be81a3ade293bd12c613fe6f4460eb
NLacuata <94291034+NLacuata@users.noreply.github.com>
2022-02-15 21:20:04 -0800

    Add files via upload

---
+++ Actuation/C++/BB_yawRoC_data.h
@@ -0,0 +1,24 @@
+/*
+ * Academic License - for use in teaching, academic research, and meeting
+ * course requirements at degree granting institutions only.  Not for
+ * government, commercial, or other organizational use.
+ *
+ * BB_yawRoC_data.h
+ *
+ * Code generation for function 'BB_yawRoC_data'
+ *
+ */
+
+#ifndef BB_YAWROC_DATA_H
+#define BB_YAWROC_DATA_H
+
+/* Include files */
+#include "rtwtypes.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/* Variable Declarations */
+extern boolean_T isInitialized_BB_yawRoC;
+
+#endif
+/* End of code generation (BB_yawRoC_data.h) */
